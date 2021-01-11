a0e0428b3389225b695d478754bedbbb3770b648
Ted M. Young <tedyoung@gmail.com>
2021-01-11 13:49:10 -0800

    Lab 2a - Created characterization test for displayFirstCard.

---
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -0,0 +1,19 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+class HandDisplayTest {
+
+  @Test
+  public void displayFirstCard() throws Exception {
+    Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
+
+    assertThat(hand.displayFirstCard())
+        .isEqualTo("\u001B[31m┌─────────┐\u001B[1B\u001B[11D│A        │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│    ♥    │\u001B[1B\u001B[11D│         │\u001B[1B\u001B[11D│        A│\u001B[1B\u001B[11D└─────────┘");
+  }
+
+}
