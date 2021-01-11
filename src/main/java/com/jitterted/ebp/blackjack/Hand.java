e350e8253dadce7fc5bfd3507d9169e6fb1a66ab
Ted M. Young <tedyoung@gmail.com>
2021-01-11 14:07:07 -0800

    Lab 2b - Extracted and moved cardsAsString to ConsoleHand.

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -2,9 +2,6 @@ package com.jitterted.ebp.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class Hand {
   private final List<Card> cards = new ArrayList<>();
@@ -25,7 +22,7 @@ public class Hand {
     // does the hand contain at least 1 Ace?
     boolean hasAce = cards
         .stream()
-        .anyMatch(card -> card.rankValue() == 1);
+        .anyMatch(card -> card.rank() == Rank.ACE);
 
     // if the total hand value <= 11, then count the Ace as 11 by adding 10
     if (hasAce && handValue <= 11) {
@@ -37,13 +34,6 @@ public class Hand {
 
   public boolean dealerMustDrawCard() {
     return value() <= 16;
-  }
-
-  public void display() {
-    System.out.println(cards().stream()
-                              .map(ConsoleCard::display)
-                              .collect(Collectors.joining(
-                               ansi().cursorUp(6).cursorRight(1).toString())));
   }
 
   public List<Card> cards() {
