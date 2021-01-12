3518ff91c4b1ac52f94aa4492f91fc31f8670523
Ted M. Young <tedyoung@gmail.com>
2021-01-12 14:07:20 -0800

    Lab 4.1 - Moved construction of Deck to application "assembler" (Blackjack).

--- src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
+++ src/main/java/com/jitterted/ebp/blackjack/Blackjack.java
@@ -1,11 +1,13 @@
 package com.jitterted.ebp.blackjack;
 
 import com.jitterted.ebp.blackjack.adapter.in.console.ConsoleGame;
+import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
 
 public class Blackjack {
   public static void main(String[] args) {
-    Game game = new Game();
+    Deck deck = new Deck();
+    Game game = new Game(deck);
     ConsoleGame consoleGame = new ConsoleGame(game);
     consoleGame.start();
   }
