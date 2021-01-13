56b06ef6c198a71d2a1e50991073d5d1ce33fcd6
Ted M. Young <tedyoung@gmail.com>
2021-01-13 12:18:12 -0800

    Implemented view of start game with view.

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -3,6 +3,7 @@ package com.jitterted.ebp.blackjack.adapter.in.web;
 import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,12 +20,15 @@ public class BlackjackController {
   @PostMapping("/start-game")
   public String startGame() {
     game.initialDeal();
-    
+
     return "redirect:/game";
   }
 
   @GetMapping("/game")
-  public String viewGame() {
+  public String viewGame(Model model) {
+    GameView gameView = GameView.of(game);
+    model.addAttribute("gameView", gameView);
+
     return "blackjack";
   }
 }
